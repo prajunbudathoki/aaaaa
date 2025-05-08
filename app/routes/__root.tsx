6e9c694409d8bdb4d1dd570f335f4aa8ac97ed79
prajun <prajunbudathoki1@gmail.com>
2025-05-08 12:55:48 +0545

    feat: :lipstick: added shadcn ui and tailwindcss

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,36 +1,42 @@
 // app/routes/__root.tsx
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
 import {
   Outlet,
   createRootRoute,
   HeadContent,
   Scripts,
-} from '@tanstack/react-router'
-
+} from "@tanstack/react-router";
+import appCss from "@/styles/app.css?url";
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        title: 'TanStack Start Starter',
+        title: "TanStack Start Starter",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
       },
     ],
   }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
@@ -44,5 +50,5 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,15 @@
 // app/routes/index.tsx
-import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  const router = useRouter();
-  const state = Route.useLoaderData();
-
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
